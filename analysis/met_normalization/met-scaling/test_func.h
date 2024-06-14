82c4b6e7a731dc9310d021e427cc67aaf9a2414c
Matt Connell <matthew.peter.connell@cern.ch>
2024-06-14 18:57:22 +0100

    Major changes: removed unnecessary code, separated scaling and summing hists into separate files, started header file

---
+++ analysis/met_normalization/met-scaling/test_func.h
@@ -0,0 +1,7 @@
+// test_func.h
+#ifndef TEST_FUNC_H
+#define TEST_FUNC_H
+
+void sum_hists(*TFile f_in, *TFile f_out, string hist_in, string hist_out, int n);  // Declaration of the function
+
+#endif
