29d27ef0213bca9a29cc13a1c8126e49743c151b
Matt Connell <matthew.peter.connell@cern.ch>
2024-06-25 00:02:18 +0100

    Small changes to main analysis plotting code
    - Commented out print lines in updateMaxEntries.cpp
    - Renamed plot_All.cpp to multi-Hist_Canvas.cpp
    - Perfected the legend, Y-axis range and title for multi-Hist_Canvas.cpp

--- analysis/includes/updateMaxEntries.cpp
+++ analysis/includes/updateMaxEntries.cpp
@@ -4,8 +4,8 @@ double updateMaxEntries(TH1D* hist, double current_max)
     Compare to the current max entries and return the larger value*/
     int maxBin = hist->GetMaximumBin();
     double maxEntries = hist->GetBinContent(maxBin);
-    cout << "Current_max: " << current_max << endl;
-    cout << "maxEntries: " << maxEntries << endl;
+    // cout << "Current_max: " << current_max << endl;
+    // cout << "maxEntries: " << maxEntries << endl;
     if (maxEntries > current_max){ current_max = maxEntries; }
     return current_max;
 }
